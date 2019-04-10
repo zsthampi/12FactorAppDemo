b5fd707f2d4ffd7045ab354fe0e1848ff7b4f916
Mapi <mappina@deloitte.com>
2019-04-09 21:40:08 -0400

    changing text for screenshot example

--- app/app.js
+++ app/app.js
@@ -2,9 +2,9 @@ const express = require('express');
 const app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.send('Hello 12 Factor App Demo!');
 });
 
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+  console.log('12FactorDemoApp listening on port 3000!');
 });
